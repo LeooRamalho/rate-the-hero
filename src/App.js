ed1a0c4594741abed84c45f89e9b6996989a2030
Leonardo Salata <lsalata@PeopleProductive.com>
2022-04-04 10:58:48 -0300

    Normalizacao de estilos / font Roboto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import {React} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {NormalizeStyles} from './shared/NormalizeStyles';
+
 import {Details} from './views/Details';
 import {NotFound} from './views/NotFound';
 import {Search} from './views/Search';
 
+
 export const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/detalhes/:id" element={<Details />} exact></Route>
-      <Route path="/" element={<Search />} exact></Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <NormalizeStyles />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/detalhes/:id" element={<Details />} exact></Route>
+        <Route path="/" element={<Search />} exact></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 );
